ca82f9da350965ad3e953a1e9b61458a0f9cb80e
anshumalviya9900 <anshumalviya9900@gmail.com>
2025-07-26 10:34:29 +0530

    Initial commit with backend and frontend

---
+++ frontend/src/Config.js
@@ -0,0 +1,5 @@
+const config = {
+  BASE_URL: "https://vastraa-backend-uh8o.onrender.com",
+};
+
+export default config;
---
+++ frontend/src/pages/DeleteProfileDetail.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import config from "../Config"; 
+
+const baseUrl = config.BASE_URL;
+const DeleteProfileDetail = () => {
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm("Are you sure you want to delete your profile?");
+    if (!confirmDelete) return;
+
+    try {
+      const res = await axios.delete(`baseUrl/profile/delete`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      toast.success(res.data.message || "Profile deleted successfully");
+    } catch (err) {
+      toast.error(err.response?.data?.error || " Failed to delete profile");
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <button onClick={handleDelete}>Delete My Profile</button>
+    </>
+  );
+};
+
+export default DeleteProfileDetail;
