0bf993fb819b5b89b8153cf6e94aafcff94b5a2b
anshumalviya9900 <anshumalviya9900@gmail.com>
2025-07-28 11:11:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
-    origin:"https://rental-dress-1.onrender.com",
+    origin:"https://rental-dress-frontend.onrender.com",
     credentials: true,
   })
 );
